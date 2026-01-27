25aa28c32a6a852b3988e5ef6e98d8a93d154e4d
Roman <romanvolovoy@gmail.com>
2026-01-27 18:06:45 +0100

    file server

--- server/server.go
+++ server/server.go
@@ -17,6 +17,11 @@ func Run() {
 	mux.HandleFunc("/api/health", okRoute)
 	mux.HandleFunc("/health", okRoute)
 
+	// TODO: handle 404
+	// ugly workaround https://github.com/denpeshkov/greenlight/blob/c68f5a2111adcd5b1a65a06595acc93a02b6380e/internal/http/middleware.go#L16-L71
+	fileServer := http.FileServer(http.Dir("dist"))
+	mux.Handle("/", fileServer)
+
 	println("starting server on port 6173")
 	err := http.ListenAndServe(":6173", mux)
 
