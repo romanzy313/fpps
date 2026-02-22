4781ebe9005530ed419c9a316ce86b79acc20dcc
Roman <romanvolovoy@gmail.com>
2026-02-22 19:26:42 +0100

    make e2e safer

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-
+  expect: {
+    timeout: 5000,
+  },
   /* Configure projects for major browsers */
   projects: [
     {
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     // environment: "jsdom",
+    include: ["web/**/*.test.ts"],
   },
   resolve: {
     alias: {
